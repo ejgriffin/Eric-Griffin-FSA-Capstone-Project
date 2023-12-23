c9afb60204370f76fd0e0b69671de6fcb735a71e
Eric Griffin <ejgriffin13@gmail.com>
2023-12-23 18:19:46 -0500

    created basic components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import "./App.css";
 import bananLogo from "./assets/bananazon.png";
 import Homepage from "./components/Homepage";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import ShoppingCart from "./components/ShoppingCart";
+import SingleProduct from "./components/SingleProduct";
 
 function App() {
   return (
@@ -10,8 +15,14 @@ function App() {
         <img id="logo-image" src={bananLogo} />
         <h1>HELLO WORLD!</h1>
       </div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
       </Routes>
     </BrowserRouter>
   );
