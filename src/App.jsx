0ce28190beb18dea73e5d9fafd6be2b088ea9139
Eric Griffin <ejgriffin13@gmail.com>
2023-12-24 20:37:19 -0500

    created some basic styling and added functional links and icons to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import bananLogo from "./assets/bananazon.png";
+
 import Homepage from "./components/Homepage";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -11,17 +11,13 @@ import SingleProduct from "./components/SingleProduct";
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <img id="logo-image" src={bananLogo} />
-        <h1>HELLO WORLD!</h1>
-      </div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<ShoppingCart />} />
-        <Route path="/cart" element={<ShoppingCart />} />
+
         <Route path="/products/:id" element={<SingleProduct />} />
       </Routes>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import bananLogo from "./assets/bananazon.png";
+import cartLogo from "./assets/cart.png";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <Link to={"/"}>
+        <img id="logo-image" src={bananLogo} />
+      </Link>
+
+      <label>
+        Search Products:
+        <input type="text" />
+      </label>
+      <Link to={"/login"}>Login</Link>
+      <Link to={"/cart"}>
+        <img id="cart-image" src={cartLogo} />
+      </Link>
+    </div>
+  );
 }
