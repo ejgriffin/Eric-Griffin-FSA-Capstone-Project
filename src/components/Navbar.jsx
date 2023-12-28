d37c2907140d64ac6880d4327f2de751eb793a46
Eric Griffin <ejgriffin13@gmail.com>
2023-12-28 10:59:59 -0500

    added additional CSS styling. created links to categories for navbar element and also link to single product page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import bananLogo from "./assets/bananazon.png";
 import cartLogo from "./assets/cart.png";
-
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <Link to={"/"}>
-        <img id="logo-image" src={bananLogo} />
-      </Link>
+    <>
+      <div className="navbar">
+        <Link to={"/"}>
+          <img id="logo-image" src={bananLogo} />
+        </Link>
 
-      <label>
-        <input type="text" style={{ width: "500px" }} />
-        <button>Search</button>
-      </label>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/cart"}>
-        <img id="cart-image" src={cartLogo} />
-      </Link>
-    </div>
+        <label>
+          <input type="text" style={{ width: "500px" }} />
+          <button>Search</button>
+        </label>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/cart"}>
+          <img id="cart-image" src={cartLogo} />
+        </Link>
+      </div>
+      <div className="categories">
+        <Link to={"*"}>Electronics</Link>
+        <Link to={"*"}>Jewelry</Link>
+        <Link to={"*"}>Men's Clothing</Link>
+        <Link to={"*"}>Women's Clothing</Link>
+      </div>
+    </>
   );
 }
--- src/components/ProductLink.jsx
+++ src/components/ProductLink.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ProductLink({ product }) {
-  const { title, image } = product;
+  const { title, image, price } = product;
   return (
-    <div>
-      <img className="productImage" src={image} alt={title} width="200"></img>
-      <h2>{title}</h2>
-    </div>
+    <Link to="/products/:id">
+      <div className="product-link">
+        <h3>{title}</h3>
+        <img className="productImage" src={image} alt={title} width="200"></img>
+        <h2>${price}</h2>
+      </div>
+    </Link>
   );
 }
