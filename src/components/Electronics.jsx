4541f9dd4a2e740f3cf2da203fa9073c95854783
Eric Griffin <ejgriffin13@gmail.com>
2024-01-16 20:55:17 -0500

    Added Electronics, Jewelry, MensClothing, and WomensClothing components

--- src/components/Electronics.jsx
+++ src/components/Electronics.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { getAllElectronics } from "../api";
 import ProductLink from "./ProductLink";
+// this is a component
 
 export default function Electronics() {
   const [electronics, setElectronics] = useState([]);
